336661218f2060926a0514d99f65381b638888e7
Matt Freitas <mattfreitas415@gmail.com>
2018-03-27 14:13:59 -0700

    cp-26-posts-users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,15 @@
 require 'random_data'
 
+#Create Users
+5.times do
+  User.create!(
+    name: RandomData.random_name,
+    email: RandomData.random_email,
+    password: RandomData.random_sentence
+  )
+end
+users = User.all
+
 #Create Topics
 15.times do
   Topic.create!(
@@ -7,12 +17,14 @@ require 'random_data'
     description: RandomData.random_paragraph
   )
 end
-topics = Topic.all 
+topics = Topic.all
 
 
 #Create Posts
 50.times do
   Post.create!(
+      user: users.sample,
+      topic: topics.sample,
       title: RandomData.random_sentence,
       body:  RandomData.random_paragraph
   )
@@ -27,6 +39,14 @@ posts = Post.all
   )
 end
 
+user = User.first
+user.update_attributes!(
+  email: 'mattfreitas415@gmail.com',
+  password: 'helloworld'
+)
+
 puts "Seed finished"
+puts "#{User.count} users created"
+puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
