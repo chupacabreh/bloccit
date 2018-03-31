615f443a93089cf73b68ba74bd47c43aeb904dac
Matt Freitas <mattfreitas415@gmail.com>
2018-03-31 11:39:12 -0700

    cp 27 complete

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180323193657) do
+ActiveRecord::Schema.define(version: 20180330190859) do
 
   create_table "comments", force: :cascade do |t|
     t.text "body"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 20180323193657) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "role"
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -39,11 +39,18 @@ posts = Post.all
   )
 end
 
-user = User.first
-user.update_attributes!(
-  email: 'mattfreitas415@gmail.com',
-  password: 'helloworld'
-)
+admin = User.create!(
+   name:     'Admin User',
+   email:    'admin@example.com',
+   password: 'helloworld',
+   role:     'admin'
+ )
+ 
+ member = User.create!(
+   name:     'Member User',
+   email:    'member@example.com',
+   password: 'helloworld'
+ )
 
 puts "Seed finished"
 puts "#{User.count} users created"
