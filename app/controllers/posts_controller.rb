9171764346476b087f5f2f64228cd6b4663545be
Matt Freitas <mattfreitas415@gmail.com>
2018-03-31 13:23:59 -0700

    assignment 27 complete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,7 @@ class PostsController < ApplicationController
 
   before_action :require_sign_in, except: :show
   before_action :authorize_user, except: [:show, :new, :create]
+  before_action :admin_or_owner, except: [:show, :new, :create, :edit, :update]
 
   def show
     @post = Post.find(params[:id])
@@ -63,9 +64,9 @@ class PostsController < ApplicationController
   def authorize_user
     post = Post.find(params[:id])
 
-    unless current_user == post.user || current_user.admin?
-      flash[:alert] = "You must be an admin to do that."
+    unless current_user == post.user || current_user.admin? || current_user.moderator?
+      flash[:alert] = "You must be an admin, moderator, or post owner to do that."
       redirect_to [post.topic, post]
     end
-  end 
+  end
 end
--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -2,7 +2,7 @@ class TopicsController < ApplicationController
 
   before_action :require_sign_in, except: [:index, :show]
   before_action :authorize_user, except: [:index, :show]
-
+  before_action :admin_only, except: [:index, :show, :update, :edit]
 
   def index
     @topics = Topic.all
@@ -62,9 +62,9 @@ class TopicsController < ApplicationController
   end
 
   def authorize_user
-    unless current_user.admin?
+    unless current_user.admin? || current_user.moderator?
       flash[:alert] = "You must be an admin to do that"
       redirect_to topics_path
     end
-  end 
+  end
 end
