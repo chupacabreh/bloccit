d6e1503b46e8749879a13cb5353ed6a9e80f7ef9
Matt Freitas <mattfreitas415@gmail.com>
2018-04-08 19:43:10 -0700

    assignment 31 complete

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,5 +1,13 @@
 module PostsHelper
   def user_is_authorized_for_post?(post)
     current_user && (current_user == post.user || current_user.admin?)
-  end 
+  end
+
+  def user_has_posts?(user)
+    user.posts.count == 0
+  end
+
+  def user_has_comments?(user)
+    user.comments.count == 0
+  end
 end
